f326cb370821270dfa53fe7f4be7365c6f1f8c2e
Arisandi <73982006+arsndii@users.noreply.github.com>
2022-07-09 01:07:48 +0800

    Add files via upload

---
+++ src/pelanggan.java
@@ -0,0 +1,6 @@
+public class pelanggan {
+
+    String nama_pelanggan, kode_jenis_pelanggan, jenis_pelanggan;
+    int jumlah_pemakaian, biaya_beban, biaya_per_m3, harga_total, pajak, total_tagihan;
+
+}
